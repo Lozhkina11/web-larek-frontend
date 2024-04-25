cdea873202b1a30fae135a51785d45573bc686c6
Lozhkina Iuliana <lozhkina_2021@list.ru>
2024-04-25 21:53:22 +0300

    types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,24 +1,27 @@
 export type Category = {
   title: string;
-  color: 'green' | 'orange' | 'blue' | 'yellow' | 'purple'
-}
+  color: 'green' | 'orange' | 'blue' | 'yellow' | 'purple';
+};
+
 export type Pill = {
   title: string;
   description: string;
   synapse: number;
   category: Category;
   imgUrl: string;
-}
+};
 
 export type Basket = {
-  shoplist: Pick<Pill, 'title' | 'synapse'>[];
+  shoplist: Pill[];
 };
 
 export type IOrderForm = {
+  typeOfPay?: boolean;
   address?: string;
   email?: string;
   phone?: string;
-}
+};
+
 export type SuccessForm = {
   price: number;
-}
+};
